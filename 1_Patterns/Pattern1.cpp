7b48b105abef0827f9e0a333f26791bcc0ffefe9
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-11-28 01:28:15 +0530

    Pattern 1 code

--- 1_Patterns/Pattern1.cpp
+++ 1_Patterns/Pattern1.cpp
@@ -1,1 +1,56 @@
- //sahil
+ #include<iostream>
+ using namespace std;
+ 
+ int main()
+ {
+    /* 1 2 3 4
+       1 2 3 4
+       1 2 3 4
+       1 2 3 4
+    */ 
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=n){
+            cout<<j<<" ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+
+ }
+
+
+
+
+ #include<iostream>
+ using namespace std;
+ 
+ int main()
+ {
+    /* 
+      4 3 2 1
+      4 3 2 1
+      4 3 2 1
+      4 3 2 1
+
+    */ 
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=n){
+            cout<<n-j+1<<" ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+
+ }
