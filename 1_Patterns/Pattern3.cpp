b79e755558051734ed346b6c30a778ec3c5b8f8e
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-11-29 15:33:01 +0530

    Added

--- 1_Patterns/Pattern3.cpp
+++ 1_Patterns/Pattern3.cpp
@@ -32,6 +32,39 @@
 // }
 
 
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     /*
+//     for n=3;
+
+//       A A A
+//       B B B
+//       C C C
+//     */
+
+//     int n;
+//     cin >> n;
+//     int i = 1;
+//     while(i <= n)
+//     {
+//         int j = 1;
+//         while (j <= n)
+//         {
+//             char ch='A'+i-1;
+//             cout <<ch<< " ";
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+//     return 0;
+// }
+
+
+
 #include <iostream>
 using namespace std;
 
@@ -40,9 +73,9 @@ int main()
     /*
     for n=3;
 
-      A A A
-      B B B
-      C C C
+      A B C
+      A B C
+      A B C
     */
 
     int n;
@@ -53,7 +86,7 @@ int main()
         int j = 1;
         while (j <= n)
         {
-            char ch='A'+i-1;
+            char ch='A'+j-1;
             cout <<ch<< " ";
             j++;
         }
