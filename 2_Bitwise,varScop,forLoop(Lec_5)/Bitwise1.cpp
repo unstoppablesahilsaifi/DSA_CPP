8ea7bc5e06e9d93a6027f2d3432d1ede234d714b
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-12-03 21:07:39 +0530

    Added

--- 2_Bitwise,varScop,forLoop(Lec_5)/Bitwise1.cpp
+++ 2_Bitwise,varScop,forLoop(Lec_5)/Bitwise1.cpp
@@ -10,5 +10,9 @@ int main()
     cout<<"a | b= "<<(a | b)<<endl;
     cout<<"~a= "<<(~a)<<endl;
     cout<<"a^b ="<<(a ^ b)<<endl;
+    cout<<(17>>1)<<endl;
+    cout<<(17>>2)<<endl;
+    cout<<(19<<1)<<endl;
+    cout<<(21<<2)<<endl;
 
 }
