1db38cce4f2d276fe8f1c9799c816a120d8cd011
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-12-03 15:38:57 +0530

    Added

--- 1_Patterns(Lec_3,4)/Pattern4_Kunal.cpp
+++ 1_Patterns(Lec_3,4)/Pattern4_Kunal.cpp
@@ -54,6 +54,36 @@
 
 
 
+//  #include<iostream>
+//  using namespace std;
+ 
+//  int main()
+//  {
+//     /* 
+//        1 
+//        1 2
+//        1 2 3
+//        1 2 3 4
+//        1 2 3 4 5 
+//     */ 
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n)
+//     {
+//         int j=1;
+//         while(j<=i){
+//             cout<<j<<" ";
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+
+//  }
+
+// 31 :16
+
  #include<iostream>
  using namespace std;
  
