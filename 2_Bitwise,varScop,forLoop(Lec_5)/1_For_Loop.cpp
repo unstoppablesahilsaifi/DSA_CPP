69b5a1b3150d75605023e3620cab8682d06aa33c
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-12-04 01:02:49 +0530

    inner outer loop output finding

--- 2_Bitwise,varScop,forLoop(Lec_5)/1_For_Loop.cpp
+++ 2_Bitwise,varScop,forLoop(Lec_5)/1_For_Loop.cpp
@@ -141,3 +141,17 @@ int main()
 }
 
 */
+
+
+/********************Find Output good question***************888*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(int i=0;i<5;i++){
+        for(int j = i;j<=5;j++){
+            cout<<i<<" "<<j<<endl;
+        }
+    }
+}
