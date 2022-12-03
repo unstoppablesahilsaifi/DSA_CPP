77aa4d0ed0170ffaa78bd8cc1565bdde213ef5d1
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-12-04 00:25:41 +0530

    added

--- 2_Bitwise,varScop,forLoop(Lec_5)/1_For_Loop.cpp
+++ 2_Bitwise,varScop,forLoop(Lec_5)/1_For_Loop.cpp
@@ -1,71 +1,75 @@
-// Pre/post Incre/Decre code
+/*
+Pre/post Incre/Decre code
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int main()
-// {
-//     int i=7;
-//     cout<<(++i)<<endl; //Pre incre
-//     cout<<(i++)<<endl; //Post Incre
-//     cout<<(i--)<<endl; // Post Decre
-//     cout<<(--i)<<endl; // Pre decre
+int main()
+{
+    int i=7;
+    cout<<(++i)<<endl; //Pre incre
+    cout<<(i++)<<endl; //Post Incre
+    cout<<(i--)<<endl; // Post Decre
+    cout<<(--i)<<endl; // Pre decre
 
-//     return 0;
-// }
+    return 0;
+}
 
+*/
 
 
 // output finding
 
+/*
+#include<iostream>
+using namespace std;
 
-// #include<iostream>
-// using namespace std;
-
-// int main()
-// {
-//     //Find output 
-//     int a, b=1;
-//     a=10;
-//     if (++a) // agar ye if condition 0 hai means false sirf tb ye else pr jayega suppose a = -1 hota aur phir ye 0 toh ye else pr jata
-//     cout<<b;
-//     else
-//     cout<< ++b;
-//     return 0;
-// }
-
+int main()
+{
+    //Find output 
+    int a, b=1;
+    a=10;
+    if (++a) // agar ye if condition 0 hai means false sirf tb ye else pr jayega suppose a = -1 hota aur phir ye 0 toh ye else pr jata
+    cout<<b;
+    else
+    cout<< ++b;
+    return 0;
+}
+*/
 
   ///////////////////////Print 1 to 5
 
   
-// #include<iostream>
-// using namespace std;
+/*
+#include<iostream>
+using namespace std;
 
-// int main()
-// {
-//     //Approach 1
-//     // int n = 5;
-//     // for(int i = 1; i<=n;i++)
-//     // {
-//     //     cout<<i<<endl;
-//     // }
-//     /////////////////or
-//     //Approach 2
-//     int n = 5;
-//     int i = 1;
-//     for(;;){
-//         if(i<=n){
-//         cout<<i<<endl;
-//         }
-//         else
-//         break;
-//         i++;
-//     }
-//     return 0;
-// }
-
+int main()
+{
+    //Approach 1
+    // int n = 5;
+    // for(int i = 1; i<=n;i++)
+    // {
+    //     cout<<i<<endl;
+    // }
+    /////////////////or
+    //Approach 2
+    int n = 5;
+    int i = 1;
+    for(;;){
+        if(i<=n){
+        cout<<i<<endl;
+        }
+        else
+        break;
+        i++;
+    }
+    return 0;
+}
+*/
 
 /////Fibonacci series 10 iteration
+/*
 #include<iostream>
 using namespace std;
 
@@ -84,3 +88,35 @@ int main()
     
     return 0;
 }
+*/
+
+/*********************Prime or not*********************/
+/*
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    bool isPrime = 1;
+
+    for(int i = 2; i<n ; i++){
+        //rem = 0, Not a Prime
+        if(n%i==0){
+             isPrime = 0;
+             break;
+        }
+    }
+     if(isPrime == 0){
+        cout<<n<<" "<<"is Not a Prime Number";
+     }
+     else{
+         cout<<n<<" "<< "is a Prime Number";
+     }
+     return 0;
+
+}
+
+*/
