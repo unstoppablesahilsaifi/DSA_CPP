e5b29568e3d2386d858e86531958d63c08977f94
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-12-03 22:51:42 +0530

    Increment/Decrement

---
+++ 2_Bitwise,varScop,forLoop(Lec_5)/1_For_Loop.cpp
@@ -0,0 +1,15 @@
+// Pre/post Incre/Decre code
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=7;
+    cout<<(++i)<<endl; //Pre incre
+    cout<<(i++)<<endl; //Post Incre
+    cout<<(i--)<<endl; // Post Decre
+    cout<<(--i)<<endl; // Pre decre
+
+    return 0;
+}
