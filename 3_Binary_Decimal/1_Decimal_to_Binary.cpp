6a1ed6f9cfb4f477bcb7e64bfb42642b2d985c49
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-12-05 15:25:11 +0530

    last bit finding

--- 3_Binary_Decimal/1_Decimal_to_Binary.cpp
+++ 3_Binary_Decimal/1_Decimal_to_Binary.cpp
@@ -5,3 +5,8 @@ Lets do this for 5
 Lets do this for 4
 4 in binary- 1 0 0 or isme hm AND & 1 krte  h to 1 0 0 aayga mtlb last digit 0 h to number even hua.
 */
+
+// Ab second kaam ki baat
+/*
+agar kisi number ki bit nikalni h to use 1 ka AND & krdo agar last bit 0 aaye to 0 thi else 1 thi
+*/
