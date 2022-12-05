e0660caf841320c52f370db3f54da5caee7be991
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-12-05 17:05:54 +0530

    dec to binary

--- 3_Binary_Decimal/1_Decimal_to_Binary.cpp
+++ 3_Binary_Decimal/1_Decimal_to_Binary.cpp
@@ -10,3 +10,26 @@ Lets do this for 4
 /*
 agar kisi number ki bit nikalni h to use 1 ka AND & krdo agar last bit 0 aaye to 0 thi else 1 thi
 */
+
+// Convert decimal to binary
+// ans = (digit*10^i)+ ans
+
+#include<iostream>
+using namespace std;
+#include<math.h>
+
+int main()
+{
+    int n;
+    cin>>n;
+    int ans=0;
+    int i=0;
+    while(n!=0){
+        int bit=n&1;
+        ans=(bit * pow(10,i)) + ans;
+        n=n >> 1;
+        i++;
+    }
+    cout<<"Answer is= "<<ans<<endl;
+    
+}
