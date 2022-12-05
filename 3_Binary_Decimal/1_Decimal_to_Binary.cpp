4ffe1bc84030d44c77ec97262e757e0eac25b054
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-12-05 15:22:06 +0530

    Odd Even finding theory

---
+++ 3_Binary_Decimal/1_Decimal_to_Binary.cpp
@@ -0,0 +1,7 @@
+/* 
+Jab ham kisi number ko AND & krte h 1 k sath or agar anser 1 aa jaye to Odd number h else even number h.
+Lets do this for 5
+5 in binary- 1 0 1 or isme hm AND & 1 krte h to 1 0 1 aayga iska mtlb last bit 1 h to number odd hua
+Lets do this for 4
+4 in binary- 1 0 0 or isme hm AND & 1 krte  h to 1 0 0 aayga mtlb last digit 0 h to number even hua.
+*/
