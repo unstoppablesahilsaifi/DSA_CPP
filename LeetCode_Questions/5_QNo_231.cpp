5ba6ec81148b5129ae66a168590ef74a3025fb55
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-12-10 16:11:51 +0530

    solution

---
+++ LeetCode_Questions/5_QNo_231.cpp
@@ -0,0 +1,19 @@
+// 231. Power of Two
+
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n) {
+//         int ans=1;
+//         int i;
+//         for(i=0;i<=30;i++){
+//             if(ans==n){
+//                 return true;
+
+//             }
+//             if(ans<INT_MAX/2){
+//                 ans=ans*2;
+//             }
+//         }
+//         return false;
+//     }
+// };
