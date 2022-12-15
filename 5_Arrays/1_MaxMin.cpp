3946ea6fa3ad97899cf868c76c193331472dfb1b
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-12-15 17:53:17 +0530

    Max program

--- 5_Arrays/1_MaxMin.cpp
+++ 5_Arrays/1_MaxMin.cpp
@@ -1,1 +1,30 @@
 // Max number finder
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int getMax(int num[], int n)
+{
+    int max = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if (num[i] > max)
+        {
+            max = num[i];
+        }
+    }
+    return max;
+}
+
+int main()
+{
+    int size;
+    cin >> size;
+    int num[100];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> num[i];
+    }
+    cout << "Maximum value is" << getMax(num, size);
+    return 0;
+}
