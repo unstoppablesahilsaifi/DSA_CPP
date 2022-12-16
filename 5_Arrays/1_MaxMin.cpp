81201e8867558d7061a5279491b3f83ce62eb9ed
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-12-16 14:48:52 +0530

    added

--- 5_Arrays/1_MaxMin.cpp
+++ 5_Arrays/1_MaxMin.cpp
@@ -5,7 +5,7 @@
 
 // int getMax(int num[], int n)
 // {
-//     int max = INT_MIN;
+//     int max = INT_MIN; //we can use here arr[0] instead of INT_MIN
 //     for (int i = 0; i < n; i++)
 //     {
 //         if (num[i] > max)
