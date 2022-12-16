88b89e18ab200013cb5a952a3f99dde8332b5f43
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-12-16 14:37:13 +0530

    Sum of the array

--- 5_Arrays/1_MaxMin.cpp
+++ 5_Arrays/1_MaxMin.cpp
@@ -1,45 +1,71 @@
-// Max number finder
+// // Max number finder
+// #include <iostream>
+// #include <climits>
+// using namespace std;
+
+// int getMax(int num[], int n)
+// {
+//     int max = INT_MIN;
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (num[i] > max)
+//         {
+//             max = num[i];
+//         }
+//     }
+//     return max;
+// }
+
+// int getMin(int num[], int n)
+// {
+//     int min = INT_MAX;
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (num[i] < min)
+//         {
+//             min = num[i];
+//         }
+//     }
+//     return min;
+// }
+
+// int main()
+// {
+//     int size;
+//     cin >> size;
+//     int num[100];
+//     for (int i = 0; i < size; i++)
+//     {
+//         cin >> num[i];
+//     }
+//     cout << "Maximum value is" << getMax(num, size)<<endl;
+//     cout << "Minimum value is" << getMin(num, size);
+
+//     return 0;
+// }
+
+// Print the sum of the array
+
 #include <iostream>
-#include <climits>
 using namespace std;
-
-int getMax(int num[], int n)
+int sum(int arr[], int n)
 {
-    int max = INT_MIN;
+    int sum = 0;
     for (int i = 0; i < n; i++)
     {
-        if (num[i] > max)
-        {
-            max = num[i];
-        }
+        sum = sum + arr[i];
     }
-    return max;
+    return sum;
 }
-
-int getMin(int num[], int n)
-{
-    int min = INT_MAX;
-    for (int i = 0; i < n; i++)
-    {
-        if (num[i] < min)
-        {
-            min = num[i];
-        }
-    }
-    return min;
-}
-
 int main()
 {
-    int size;
-    cin >> size;
-    int num[100];
-    for (int i = 0; i < size; i++)
+    int arr[50];
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
     {
-        cin >> num[i];
+        cin >> arr[i];
     }
-    cout << "Maximum value is" << getMax(num, size)<<endl;
-    cout << "Minimum value is" << getMin(num, size);
-
+    cout << "Sum of the array is" << sum(arr, n);
     return 0;
 }
