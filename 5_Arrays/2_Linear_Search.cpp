028c28e5caab6e25f2f742ef3254f0c8d19a3e70
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-12-17 17:11:03 +0530

    My  Approach

--- 5_Arrays/2_Linear_Search.cpp
+++ 5_Arrays/2_Linear_Search.cpp
@@ -1,1 +1,32 @@
 // Linear Search Program in C++
+#include <iostream>
+using namespace std;
+int check(int arr[], int n)
+{    
+    int v;
+    cout << "no is to  be checked";
+    cin >> v;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i]==v)
+        {
+            return 1;
+        }
+      
+    }
+      
+            return 0;
+}
+int main()
+{
+
+    int arr[100];
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout<<"No is"<< check(arr,n);
+    return 0;
+}
