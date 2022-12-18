f358eeb63997e61bceee07dd46c480c67eeeb24c
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-12-18 14:50:13 +0530

    best approach for power of two

---
+++ GeeksForGeeks/1_PowerOfTwo.cpp
@@ -0,0 +1,22 @@
+
+
+// class Solution{
+//     public:
+//     // Function to check if given number n is a power of two.
+//     bool isPowerofTwo(long long n){
+        
+//         // Your code here 
+//       if(n==0){
+//           return 0;
+//       }
+//     if ((n&(n-1))==0)
+//     {
+//         return 1;
+//     }
+//     else 
+//     {
+//       return 0;
+//     }
+        
+//     }
+// };
