3399d077eec21f27d45d6c2f5242719f022fa21a
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-12-19 02:38:12 +0530

    With Rishabh but not able to submit coz of optimz

--- GeeksForGeeks/1_BitMagic/2_CountTotalSetBit.cpp
+++ GeeksForGeeks/1_BitMagic/2_CountTotalSetBit.cpp
@@ -1,4 +1,4 @@
-// My Approach
+// My Approach for the normal program to find the no of bits of any number
 
 
 // class Solution{
@@ -13,8 +13,48 @@
 //             if(n&1){
 //                 count++;
 //             }
-//             n>>1;
+//             n=n>>1;
 //         }
 //         return count;
 //     }
 // };
+
+
+/*
+You are given a number N. Find the total count of set bits for all numbers from 1 to N(both inclusive).
+Input: N = 4
+Output: 5
+Explanation:
+For numbers from 1 to 4.
+For 1: 0 0 1 = 1 set bits
+For 2: 0 1 0 = 1 set bits
+For 3: 0 1 1 = 2 set bits
+For 4: 1 0 0 = 1 set bits
+Therefore, the total set bits is 5.
+*/
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter n";
+    cin>>n;
+        // Your logic here
+        int count=0;
+        for(int i=1;i<=n;i++)
+        {
+            int x = i;
+        while(x!=0)
+        {
+        if(x&1)
+        {
+         count++;
+        }
+        x=x>>1;
+        }
+        }
+        cout<<count;
+}
