15165a393b3929de546549fa1f996e07a2d8ff7c
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-12-20 13:30:15 +0530

    ok

--- 5_Arrays/1_MaxMin.cpp
+++ 5_Arrays/1_MaxMin.cpp
@@ -72,29 +72,28 @@
 
 // second approch to check whether the number can be defined in the power of 2 or not.
 
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    int n;
-    cin >> n;
-    int count = 0;
-    while (n != 0)
-    {
-        if (n & 1)
-        {
-            count++;
-        }
-        n = n >> 1;
-    }
-    if (count == 1)
-    {
-        cout<< "can be defined in the power of two";
-    }
-    else
-    {
-        cout << "can not be defined in the power of 2";
-    }
-    return 0;
-}
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int count = 0;
+//     while (n != 0)
+//     {
+//         if (n & 1)
+//         {
+//             count++;
+//         }
+//         n = n >> 1;
+//     }
+//     if (count == 1)
+//     {
+//         cout << "can be defined in the power of two";
+//     }
+//     else
+//     {
+//         cout << "can not be defined in the power of 2";
+//     }
+//     return 0;
+// }
