30f405e9bd2580a64622cdf3165cca2c461ebce1
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2022-12-27 15:21:51 +0530

    ok

--- CodeStudio/2_FindDuplicate.cpp
+++ CodeStudio/2_FindDuplicate.cpp
@@ -34,16 +34,16 @@
 
 
 ///// Optimize Approach/////
-#include <bits/stdc++.h> 
-#include <bits/stdc++.h> 
-int findDuplicate(vector<int> &arr, int n){
-	// Write your code here.
-     int ans=0;
-    for(int i=0;i<arr.size();i++){
-        ans=ans^arr[i];
-    }
-     for(int i=1;i<arr.size();i++){
-        ans=ans^i;
-     }
-    return ans;
-}
+// #include <bits/stdc++.h> 
+// #include <bits/stdc++.h> 
+// int findDuplicate(vector<int> &arr, int n){
+// 	// Write your code here.
+//      int ans=0;
+//     for(int i=0;i<arr.size();i++){
+//         ans=ans^arr[i];
+//     }
+//      for(int i=1;i<arr.size();i++){
+//         ans=ans^i;
+//      }
+//     return ans;
+// }
