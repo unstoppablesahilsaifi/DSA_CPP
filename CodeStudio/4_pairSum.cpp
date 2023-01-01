1963d37c12d1dd8bd40a5df59c655aa5b4725bae
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2023-01-01 17:22:24 +0530

    ok

--- CodeStudio/4_pairSum.cpp
+++ CodeStudio/4_pairSum.cpp
@@ -5,21 +5,21 @@
 
 // vector<vector<int>> pairsum(vector<int> &arr, int s)
 // {
-//     vector<vector<int>> ans;
-//     sort(arr.begin(), arr.end());
-//     for (int i = 0; i < arr.size(); i++)
-//     {
-//         for (int j = i + 1; j < arr.size(); j++)
-//             if (arr[i] + arr[j] == s)
-//             {
+    // vector<vector<int>> ans;
+    // sort(arr.begin(), arr.end());
+    // for (int i = 0; i < arr.size(); i++)
+    // {
+    //     for (int j = i + 1; j < arr.size(); j++)
+    //         if (arr[i] + arr[j] == s)
+    //         {
 
-//                 vector<int> temp;
-//                 temp.push_back(arr[i]);
-//                 temp.push_back(arr[j]);
-//                 ans.push_back(temp);
-//             }
-//     }
-//     return ans;
+    //             vector<int> temp;
+    //             temp.push_back(arr[i]);
+    //             temp.push_back(arr[j]);
+    //             ans.push_back(temp);
+    //         }
+    // }
+    // return ans;
 // }
 
 // int main()
@@ -39,21 +39,21 @@
 
 // vector<vector<int>> pairsum(vector<int> &arr, int s)
 // {
-//     vector<vector<int>> ans;
-//     for (int i = 0; i < arr.size(); i++)
-//     {
-//         for (int j = i + 1; j < arr.size(); j++)
-//             if (arr[i] + arr[j] == s)
-//             {
+    // vector<vector<int>> ans;
+    // for (int i = 0; i < arr.size(); i++)
+    // {
+    //     for (int j = i + 1; j < arr.size(); j++)
+    //         if (arr[i] + arr[j] == s)
+    //         {
 
-//                 vector<int> temp;
-//                 temp.push_back(min(arr[i],arr[j]));
-//                 temp.push_back(max(arr[i],arr[j]));
-//                 ans.push_back(temp);
-//             }
-//     }
-//     sort(ans.begin(),ans.end());
-//     return ans;
+    //             vector<int> temp;
+    //             temp.push_back(min(arr[i],arr[j]));
+    //             temp.push_back(max(arr[i],arr[j]));
+    //             ans.push_back(temp);
+    //         }
+    // }
+    // sort(ans.begin(),ans.end());
+    // return ans;
 // }
 
 // int main()
