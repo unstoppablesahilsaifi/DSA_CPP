c6fd1c59ca1d35c59af226b6288e77a2d62e6989
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2023-01-13 17:41:44 +0530

    ok

--- All_Type_Questions/cosecutiveSumFinder.cpp
+++ All_Type_Questions/cosecutiveSumFinder.cpp
@@ -15,3 +15,25 @@ Only 12 and 2 can be represented in a form n*(n+1)
 -> 12= 3*4 or 3*(3+1)
 -> 2=  1*2 or 1*(1+1)
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {134, 12, 7, 2, 1324};
+    int n = 5;
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i] == j * (j + 1))
+            {
+                count++;
+            }
+        }
+    }
+    cout<<count;
+    return 0;
+}
