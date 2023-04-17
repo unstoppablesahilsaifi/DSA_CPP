3189f705a98ada78acde20547d21d0c72e699fbc
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2023-04-18 00:44:01 +0530

    corrected code

--- 7_BinarySearch/4_mountainPeak.cpp
+++ 7_BinarySearch/4_mountainPeak.cpp
@@ -1,25 +1,25 @@
-// Prod to find max element using Bog O(n)
-#include <iostream>
-using namespace std;
-int maxFinder(int arr[], int n)
-{
-    int ans = arr[0];
-    for (int i = 1; i < n; i++)
-    {
-        if (ans < arr[i])
-        {
-            ans=arr[i];
-        }
-    }
-    return ans;
-}
-int main()
-{
-    int arr[] = {1, 6, 9, 12, 16, 17, 14, 11, 0,500};
-    int n = 10;
-    cout << "Max Number" << maxFinder(arr, n);
-    return 0;
-}
+// // Prod to find max element using Bog O(n)
+// #include <iostream>
+// using namespace std;
+// int maxFinder(int arr[], int n)
+// {
+//     int ans = arr[0];
+//     for (int i = 1; i < n; i++)
+//     {
+//         if (ans < arr[i])
+//         {
+//             ans=arr[i];
+//         }
+//     }
+//     return ans;
+// }
+// int main()
+// {
+//     int arr[] = {1, 6, 9, 12, 16, 17, 14, 11, 0,500};
+//     int n = 10;
+//     cout << "Max Number" << maxFinder(arr, n);
+//     return 0;
+// }
 
 // ---------------------------------------------------------------
 
@@ -57,3 +57,32 @@ int main()
     cout<<"index of peak is: "<<findPeak(arr, n);
     return 0;
 }
+
+
+-------------------------------------------------------------
+
+//Leetcode solution
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int start = 0;
+    int end =arr.size()-1;
+    int mid = start +(end-start)/2;
+  
+
+    while(start<end){
+        if(arr[mid]<arr[mid+1]){
+            start = mid+1;
+        }
+        else{
+             end = mid;
+        }
+       
+        mid = start +(end-start)/2;
+        
+    }
+return start;
+
+    }
+};
