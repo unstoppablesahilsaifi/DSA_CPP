243fee92a6cf5071fe20049693b82573f8020cbe
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2023-05-20 13:48:53 +0530

    add

--- 8_Sorting/1_SelectionSortCode.cpp
+++ 8_Sorting/1_SelectionSortCode.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
 
-void selectionSort(int arr[], int n)
+void selectionSort(int arr[])
 {
-    
+   int n = sizeof(arr);
     for(int i=0;i<n-1;i++){
         int minIndex=i;
         for(int j=i+1;j<n;j++){
@@ -22,7 +22,7 @@ int main()
     int arr[10]={22,45,11,3,86};
     int n=5;
     
-    selectionSort(arr,n);
+    selectionSort(arr);
 
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
