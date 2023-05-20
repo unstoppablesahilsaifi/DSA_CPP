5c9707faf930461fbce7d244dece4b8633c556d1
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2023-05-20 13:33:11 +0530

    add

--- 8_Sorting/1_SelectionSortCode.cpp
+++ 8_Sorting/1_SelectionSortCode.cpp
@@ -6,7 +6,7 @@ void selectionSort(int arr[], int n)
     
     for(int i=0;i<n-1;i++){
         int minIndex=i;
-        for(int j=i+1;i<n;j++){
+        for(int j=i+1;j<n;j++){
             if(arr[j]<arr[minIndex])
                 minIndex=j;
            
@@ -21,7 +21,9 @@ int main()
 {
     int arr[10]={22,45,11,3,86};
     int n=5;
+    
     selectionSort(arr,n);
+
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
