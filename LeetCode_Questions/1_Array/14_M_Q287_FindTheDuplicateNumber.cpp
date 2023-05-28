a4b799782619e71851d4bd4a6813d7c6630e66d7
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2023-05-28 12:53:52 +0530

    added

--- LeetCode_Questions/1_Array/14_M_Q287_FindTheDuplicateNumber.cpp
+++ LeetCode_Questions/1_Array/14_M_Q287_FindTheDuplicateNumber.cpp
@@ -12,3 +12,20 @@ public:
         return -1;
     }
 };
+
+//Second approach using XOR
+
+
+class Solution {
+public:
+    // Using XOR 
+int findDuplicate(vector<int> &nums) {
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++)
+        for (int j = i + 1; j < n; j++)
+            if ((nums[i] ^ nums[j]) == 0)
+                return nums[i];
+    return -1;
+}
+};
