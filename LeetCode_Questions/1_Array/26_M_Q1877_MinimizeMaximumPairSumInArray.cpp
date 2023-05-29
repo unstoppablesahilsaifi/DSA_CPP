d5131f816e2bdbcab024cd6b606e640d70280ff4
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2023-05-29 15:31:43 +0530

    added

---
+++ LeetCode_Questions/1_Array/26_M_Q1877_MinimizeMaximumPairSumInArray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+           sort(nums.begin(),nums.end());
+        int res = 0  , i = 0, j = nums.size()-1; 
+        while(i<j){
+            res = max(res,nums[i]+nums[j]);
+            i++;
+            j--;
+        }
+        return res; 
+    }
+};
