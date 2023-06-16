ab7a6dac1d770e6fdcc5c7869b1a9f4b985259b1
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2023-06-16 18:35:21 +0530

    code added

--- 8_Sorting/3_InsertionSortCode.cpp
+++ 8_Sorting/3_InsertionSortCode.cpp
@@ -1,1 +1,24 @@
 // Code is below
+
+#include<vector>
+using namespace std;
+
+void insertionSort(int n, vector<int> &arr){
+    
+    for(int i = 1; i<n; i++) {
+        int temp = arr[i];
+        for(Int j=i-1; j>=0; j--) {
+            
+            if(arr[j] > temp) {
+                //shift
+                arr[j+1] = arr[j];
+            }
+            else { // ruk jao
+                break;
+            }
+            
+        }
+        //copy temp value
+        arr[j+1] = temp;  
+    } 
+}
