97098344578007d4ea737e57ffcc84cfc9a2cae6
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2023-06-27 15:43:12 +0530

    added

---
+++ LeetCode_Questions/2_String_Char/4_M_Q567_PermutationInString.cpp
@@ -0,0 +1,54 @@
+class Solution {
+private:
+    bool checkEqual(int a[26], int b[26]) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+public:
+    bool checkInclusion(string s1, string s2) {
+        // Character count array
+        int count1[26] = {0};
+        for (int i = 0; i < s1.length(); i++) {
+            int number = s1[i] - 'a';
+            count1[number]++;
+        }
+
+        // Traverse s2 string in a window of size s1 length and compare
+        int i = 0;
+        int windowSize = s1.length();
+        int count2[26] = {0};
+        // Running for the first window
+        while (i < windowSize &&i < s2.length()) {
+            int index = s2[i] - 'a';
+            count2[index]++;
+            i++;
+        }
+
+        if (checkEqual(count1, count2)) {
+            return true;
+        }
+
+        // Process the rest of the windows
+        while (i < s2.length()) {
+            char newChar = s2[i];
+            int index = newChar - 'a';
+            count2[index]++;
+
+            char oldChar = s2[i - windowSize];
+            index = oldChar - 'a';
+            count2[index]--;
+            i++;
+
+            if (checkEqual(count1, count2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
