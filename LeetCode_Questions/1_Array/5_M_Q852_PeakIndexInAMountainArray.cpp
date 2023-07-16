a2fedac4e0b9133f477b83ce83570060494f48e1
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2023-07-16 19:38:11 +0530

    added

--- LeetCode_Questions/1_Array/5_M_Q852_PeakIndexInAMountainArray.cpp
+++ LeetCode_Questions/1_Array/5_M_Q852_PeakIndexInAMountainArray.cpp
@@ -5,12 +5,14 @@ public:
     int end =arr.size()-1;
     int mid = start +(end-start)/2;
   
-
+//In case agar start and end peeak mountain per h to fir bhi ye loop k anday jayga agar equal lgate h to.
+//isliye equal  nhi lgaya
     while(start<end){
         if(arr[mid]<arr[mid+1]){
             start = mid+1;
         }
         else{
+            //In case, agar peak element hi mid per aa gya to mid-1 s to piche iterate ho  jaynge hm isliye mid hi thik h
              end = mid;
         }
        
