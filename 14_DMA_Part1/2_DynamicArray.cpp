0156156e872cf2f99bc6a752e23f08611d62ad27
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2023-08-19 22:07:27 +0530

    added

--- 14_DMA_Part1/2_DynamicArray.cpp
+++ 14_DMA_Part1/2_DynamicArray.cpp
@@ -42,3 +42,20 @@ int main() {
 
     return 0;
 }
+
+
+/*
+
+BAD PRACTICE::
+
+
+Jab bhi kabhi array ka size pta nhi ho and user s size lena ho to kuch bhi max size eg; arr[10000] dedenaa per arr[n] nhi krna .
+
+Why:   
+
+Jab bhi program start hota h to apne sath memory leke aata h, us memory m uske paas do type ki memory hoti h Stack(Choti) and Heap(Badi).
+agar m compile time per hi bata de ki mujhe 10000 size ka array chahiey to program stack ki memory usi hisab s leke aiiyo.
+
+lekin agar hm ye bol de ki arr[n] to vo choti memory leke aayga stack k hisab s and hme array bnana h bdaa size ka,
+Run time per array ka size hm  bataynge jyada but program choti memory leke aaya h to program crash kr jaayga
+*/
