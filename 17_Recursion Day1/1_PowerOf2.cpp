2d3aaed8a7187c924142e2cb4becb9a59492d9a0
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2023-08-26 23:27:07 +0530

    added

--- 17_Recursion Day1/1_PowerOf2.cpp
+++ 17_Recursion Day1/1_PowerOf2.cpp
@@ -1,1 +1,28 @@
 // Agar kisi badi problem ka solution depend krta h kisi same type ki choti problem per to ham recursion use krte h 
+
+// 2 ki power n= 2 * 2 ki power n-1
+
+#include<iostream> 
+using namespace std;
+
+int power(int n) {
+
+    //base case 
+    if(n == 0)
+        return 1;
+
+    //recursive relation
+    return 2 * power(n-1);    
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int ans = power(n);
+
+    cout << ans << endl;
+
+    return 0;
+}
