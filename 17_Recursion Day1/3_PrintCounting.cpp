273d9b6a6678370a199df46fff1acf35d447dc1e
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2023-08-30 00:59:30 +0530

    Update 3_PrintCounting.cpp
    ok

--- 17_Recursion Day1/3_PrintCounting.cpp
+++ 17_Recursion Day1/3_PrintCounting.cpp
@@ -1,1 +1,23 @@
 //Code
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+void printCounting(int n){
+    if(n==0){
+        return;
+    }
+    printCounting(n-1); // Added a semicolon here
+    std::cout << n;    // Added a space before n
+}
+
+int main() {
+    // Write C++ code here
+    std::cout << "Hello world!";
+    int n = 6;
+    printCounting(n);
+
+    return 0;
+}
+
+
+
