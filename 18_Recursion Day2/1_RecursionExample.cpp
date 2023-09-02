fb7d90e3dd668d17bb7ae26692b953af6bf13a1f
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2023-09-03 00:32:20 +0530

    added

--- 18_Recursion Day2/1_RecursionExample.cpp
+++ 18_Recursion Day2/1_RecursionExample.cpp
@@ -1,1 +1,32 @@
 // Code
+#include<iostream>
+using namespace std;
+void reachHome(int src, int destination){
+     
+
+     cout<<"Source  "<<src<<"  Destination  "<<destination<<endl;
+     //Base case
+     if(src==destination){
+        cout<<"Phuch  Gya"<<endl;
+        //return statement mandate  hoti h base case m
+        return ;
+     }
+
+     //Recursive call
+
+     src++;
+
+     reachHome(src,destination);
+
+}
+
+int main(){
+
+   int destination=10;
+   int src=1;
+
+   reachHome(src,destination);
+
+    return 0;
+
+}
