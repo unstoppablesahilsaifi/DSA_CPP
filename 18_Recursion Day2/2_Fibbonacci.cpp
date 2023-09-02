b84852e04318d03a8c7a725a82da3222e55e0646
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2023-09-03 00:51:03 +0530

    added

--- 18_Recursion Day2/2_Fibbonacci.cpp
+++ 18_Recursion Day2/2_Fibbonacci.cpp
@@ -1,13 +1,18 @@
 // Code to find the nth term of fibonacci series
 // 0 1 1 2 3 5 8 13 21............
-#include<iostream>
-using namespace std;
 
+//LeetCode 509 Question
 
-int main(){
+  int fib(int n) {
+        //Base Case
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        //Recursive Call
 
-    
-    
-    return 0;
-
-}
+        int ans=fib(n-1)+fib(n-2);
+        return ans;
+    }
