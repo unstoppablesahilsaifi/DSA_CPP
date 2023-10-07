fabfeb3469117444bdd29a3f6ede837dbc96bfcb
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2023-10-08 01:52:04 +0530

    added

--- 19_Recursion Day3/3_LinearSearch.cpp
+++ 19_Recursion Day3/3_LinearSearch.cpp
@@ -16,10 +16,13 @@ bool linearSearch(int arr[], int size, int k ) {
     if(size == 0) 
         return false;
 
+
+    // agar aaray ka 1st element key h to true krdo nhi to baaki(else part) array m check kro na bhai fir
     if(arr[0] == k) {
         return true;
     }
     else {
+        // ab yhaa per arr+1 krdiya h iska mtlb dusra index as 1st element check hoga if condition m
         bool remainingPart = linearSearch(arr+1, size-1, k );
         return remainingPart;
     }
