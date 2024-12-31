5a33ac42f05ddc28ba606675d388c56a60e97c8e
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2024-12-31 13:03:14 +0530

    added

--- LeetCode_Questions/5_QNo_231.cpp
+++ LeetCode_Questions/5_QNo_231.cpp
@@ -17,3 +17,31 @@
 //         return false;
 //     }
 // };
+
+
+/*
+#include <iostream>
+using namespace std;
+
+bool isPowerOfTwo(int n) {
+    if (n <= 0) {
+        return false; // Powers of two are positive integers
+    }
+    return (n & (n - 1)) == 0;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isPowerOfTwo(num)) {
+        cout << num << " is a power of two." << endl;
+    } else {
+        cout << num << " is not a power of two." << endl;
+    }
+
+    return 0;
+}
+
+*/
