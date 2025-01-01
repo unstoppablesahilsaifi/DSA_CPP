61ead8ec6ec5d24c3d020adfbf370d1ace185cf4
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2025-01-01 18:22:59 +0530

    added

--- LeetCode_Questions/1_Array/27_M_Q442_FindAllDuplicateInAnArray.cpp
+++ LeetCode_Questions/1_Array/27_M_Q442_FindAllDuplicateInAnArray.cpp
@@ -13,3 +13,27 @@ public:
         return ans;
     }
 };
+
+/*
+Brute force nut gave TLE
+
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        for (int i=0;i<nums.size();i++){
+            for (int j=0;j<nums.size();j++){
+                if (i==j){
+                    j++;
+                    break;
+                }
+                if(nums[i]==nums[j]){
+                    ans.push_back(nums[i]);
+                }
+            }
+        }
+        return ans;
+    }
+};
+*/
