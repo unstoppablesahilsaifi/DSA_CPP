7984bb1d58e731d88fa54ec6153137886681a6e6
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2025-01-01 19:11:24 +0530

    added

--- CodeStudio/6_Sort_0_1.cpp
+++ CodeStudio/6_Sort_0_1.cpp
@@ -44,3 +44,51 @@ int main()
     sort(arr, size);
     return 0;
 }
+
+
+/*
+#include <iostream>
+using namespace std;
+
+void sortBinaryArray(int arr[], int n) {
+    int left = 0; // Pointer to the start of the array
+    int right = n - 1; // Pointer to the end of the array
+    
+    while (left < right) {
+        if (arr[left] == 0) {
+            left++; // Move left pointer to the right
+        } else if (arr[right] == 1) {
+            right--; // Move right pointer to the left
+        } else {
+            // Swap arr[left] and arr[right] if arr[left] is 1 and arr[right] is 0
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
+        }
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    
+    int arr[n];
+    cout << "Enter the elements (0 or 1):\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    sortBinaryArray(arr, n);
+    
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
+
+
+*/
