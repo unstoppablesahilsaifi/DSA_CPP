2e8b37b180a8feba112356c72ebed0e5b54cb53c
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2025-01-03 15:13:16 +0530

    added

--- 7_BinarySearch/7_BookAlllocation.cpp
+++ 7_BinarySearch/7_BookAlllocation.cpp
@@ -5,6 +5,10 @@ You are given an array arr[] of integers, where each element arr[i] represents t
 Each student receives atleast one book.
 Each student is assigned a contiguous sequence of books.
 No book is assigned to more than one student.
+
+
+
+The objective is to minimize the maximum number of pages assigned to any student. In other words, out of all possible allocations, find the arrangement where the student who receives the most pages still has the smallest possible maximum.
 */
 
 
