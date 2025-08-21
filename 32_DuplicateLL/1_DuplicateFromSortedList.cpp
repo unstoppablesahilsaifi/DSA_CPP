57f9da53625b0f75e79904ff54366de2d3655b2e
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2025-08-21 12:31:07 +0530

    added

---
+++ 32_DuplicateLL/1_DuplicateFromSortedList.cpp
@@ -0,0 +1,24 @@
+// class Solution {
+// public:
+//     ListNode* deleteDuplicates(ListNode* head) {
+//         // empty list
+//         if (head == NULL) {
+//             return NULL;
+//         }
+
+//         // Non-Empty List
+//         ListNode* curr = head;
+//         while (curr != NULL && curr->next != NULL) {
+//             if (curr->val == curr->next->val) {
+//                 // duplicate mila → next ko skip karo
+//                 ListNode* dup = curr->next; 
+//                 curr->next = curr->next->next;
+//                 delete dup; // memory free
+//             } else {
+//                 // unique node → aage badho
+//                 curr = curr->next;
+//             }
+//         }
+//         return head;
+//     }
+// };
