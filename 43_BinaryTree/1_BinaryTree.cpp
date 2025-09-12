9ffa72fc5cf05d132219fe1fa1ac65b3255aa549
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2025-09-12 15:36:50 +0530

    added

---
+++ 43_BinaryTree/1_BinaryTree.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class node {
+    public:
+        int data;
+        node* left;
+        node* right;
+
+    node(int d) {
+        this -> data = d;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+};
+
+node* buildTree(node* root) {
+
+    cout << "Enter the data: " << endl;
+    int data;
+    cin >> data;
+    root = new node(data);    
+
+    if(data == -1) {
+        return NULL;
+    }
+
+    cout << "Enter data for inserting in left of " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter data for inserting in right of " << data << endl;
+    root->right = buildTree(root->right);
+    return root;
+
+}
+
+int main() {
+
+    node* root = NULL;
+
+   
+    // 1 3 5 7 11 17 -1 -1 -1 -1 -1 -1 -1
+
+    
+    //creating a Tree
+    root = buildTree(root);
+
+
+
+    return 0;
+}
